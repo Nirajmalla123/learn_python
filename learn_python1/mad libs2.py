5e2c18e590940aa2529fb4a8421d5dd25f057f88
Niraj Malla <186814009+Nirajmalla123@users.noreply.github.com>
2026-07-07 20:19:54 +0545

    Add files via upload

---
+++ learn_python1/mad libs2.py
@@ -0,0 +1,22 @@
+name=input("Enter your name:")
+age = int(input("Enter your age:"))
+print(f"hi {name}")
+print(f"your are {age} years old")
+
+
+# mad libs
+Nam=input ("enter the name:")
+verb=input("enter the verb:")
+food=input("enter the food:")
+adjective=input("enter the adfective:")
+bodypart=input("enter the part:")
+
+print(f"Today,{Nam} accidntally {verb} their {food} into a {adjective} teacher’s {bodypart}.")
+print("Now they're banned from the cafeteria forever.")
+
+
+#area calculator
+length = int(input("enter the length of the rectangle:"))
+breadth=int(input("enter the bredth of the rectangle:"))
+area = length * breadth
+print(f"the area of the rectangle is {area}") 
