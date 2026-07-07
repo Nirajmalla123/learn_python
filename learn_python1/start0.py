618f95042ae454acb64b9d0d0665fcbbdc709d39
Niraj Malla <186814009+Nirajmalla123@users.noreply.github.com>
2026-07-07 20:11:48 +0545

    Add files via upload

---
+++ learn_python1/start0.py
@@ -0,0 +1,23 @@
+#  Print
+print("i am a good boy")
+print("Hello World!")
+
+#variable = a container for a value (string,integer,float,boolean)
+#          a variable behave as if it was the value it contains
+   
+   #string
+first_name = "Niraj"
+print(first_name)
+print(f"hello {first_name} ")
+#integer
+age = 24 
+print (f"you are {age} years old.")
+#float = is a number but contain a decimal portion
+price = 24.34
+print (f"the price is {price} rupee")
+#boolean = is a either true or a false
+is_student = True
+if is_student:
+ print("you are a student")
+else: 
+ print("you are not a student")
